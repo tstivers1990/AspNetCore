3b75edd75b4fb245dd2742fe08e2edf5a340510d
Javier Calvarro Nelson <jacalvar@microsoft.com>
2019-09-02 07:58:01 -0700

    Cleanup error handling tests

--- src/Components/test/testassets/TestServer/Startup.cs
+++ src/Components/test/testassets/TestServer/Startup.cs
@@ -1,8 +1,6 @@
 using System.Threading.Tasks;
-using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Localization;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
@@ -21,24 +19,12 @@ namespace TestServer
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
-            services.AddMvc().AddNewtonsoftJson();
+            services.AddMvc();
             services.AddCors(options =>
             {
                 options.AddPolicy("AllowAll", _ => { /* Controlled below */ });
             });
-            services.AddServerSideBlazor()
-                .AddCircuitOptions(o =>
-                {
-                    var detailedErrors = Configuration.GetValue<bool>("circuit-detailed-errors");
-                    o.DetailedErrors = detailedErrors;
-                });
-            services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme).AddCookie();
-
-            services.AddAuthorization(options =>
-            {
-                options.AddPolicy("NameMustStartWithB", policy =>
-                    policy.RequireAssertion(ctx => ctx.User.Identity.Name?.StartsWith("B") ?? false));
-            });
+            services.AddServerSideBlazor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
